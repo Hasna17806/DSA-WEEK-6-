a4580b5dbb44168023834ff3a689fad463685997
Hasna17806 <hasnahamza807@gmail.com>
2026-06-04 17:53:32 +0530

    second commit

--- day1.js
+++ day1.js
@@ -1,7 +1,26 @@
 //////////////////////////stack traversal////////////////
 
-let stack =  [10, 20, 30, 40];
+// let stack =  [10, 20, 30, 40];
 
-for (let i = stack.length - 1; i >= 0; i--){
-    console.log(stack[i]);
-}
+// for (let i = stack.length - 1; i >= 0; i--){
+//     console.log(stack[i]);
+// }
+
+/////////////////////Build stack using array////////////////////////////
+
+// class stack{
+//     constructor() {
+//         this.item = [];
+//     }
+
+//     push(value){
+//         this.item.push(value);
+//     }
+// }
+
+// let stack = new Stack();
+
+// stack.push(10);
+// stack.push(20);
+// stack.push(30);
+
---
+++ day2.js
@@ -0,0 +1,22 @@
+///////////////Queue using array/////////////////////
+
+// let queue = new Queue();
+
+// queue.enqueue(5);
+// queue.enqueue(10);
+// queue.enqueue(15);
+
+// console.log(queue.front());
+
+
+let queue = new Queue();
+
+queue.enqueue(5);
+queue.enqueue(10);
+queue.enqueue(15);
+
+console.log(queue.front());
+
+queue.dequeue();
+
+console.log(queue.front());
